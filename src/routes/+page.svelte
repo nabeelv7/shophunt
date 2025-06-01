1d1365883b13aa569198f1c8acaed4f8446bd80b
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-01 18:23:45 +0500

    started

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>ShopHunt - Discover the Best Shopping Sites, All in One Place</title>
+  <meta
+    name="description"
+    content="Explore, rate, and share your favorite online shops ~ so you never miss out on the next amazing deal."
+  />
+</svelte:head>
